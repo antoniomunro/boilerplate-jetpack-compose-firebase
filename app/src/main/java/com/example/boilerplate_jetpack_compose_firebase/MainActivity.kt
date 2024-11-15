3673387b63af206a35b6e610b2d1931f980b271b
aantoniomunro <aantoniomunro@gmail.com>
2024-11-15 16:14:45 +1300

    Remote Connection

---
+++ app/src/main/java/com/example/boilerplate_jetpack_compose_firebase/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.boilerplate_jetpack_compose_firebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.boilerplate_jetpack_compose_firebase.ui.theme.BoilerplatejetpackcomposefirebaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BoilerplatejetpackcomposefirebaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BoilerplatejetpackcomposefirebaseTheme {
+        Greeting("Android")
+    }
+}
